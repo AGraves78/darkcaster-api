515bda7ed95ae2d57aac640f210c77568024fa0e
Angela Graves <algraves78@gmail.com>
2017-06-29 18:31:12 -0400

    successfully created the /weather route that returns weather data for Gainesville

--- routers/weather.router.js
+++ routers/weather.router.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+const darksky = require('../credentials').darksky; //be careful!!!
+const baseUrl = `https://api.darksky.net/forecast/${darksky}/37.8267,-122.4233`;
+const axios = require('axios');
 router.get('/weather', (request, response) => {
-  response.send('Here be the weather');
+const url = `${baseUrl}29, -82`;
+  axios.get(baseUrl)
+        .then(weather => {
+          response.json(weather.data);
+        })
+        .catch(err => {
+          console.error(err);
+        })
 });
 router.get('/weather/:lat,:lon', (request, response) => {
   response.send('Here be the weather for that place');
